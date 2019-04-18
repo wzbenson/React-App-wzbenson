d85b69da2e4096fa88f9b2b90aa5e6c4a623d2cd
Zach Benson <zachzikool101@gmail.com>
2019-04-18 14:33:00 -0600

    edit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : null
+    }
+  }
   render() {
     return (
       <button className="square"
-        onClick={()=>alert('click')}>
-        {this.props.number}
+        onClick={() => this.setState({value:'X'})}>
+        {this.state.value}
       </button>
     );
   }
@@ -15,7 +21,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square number={i} />;
+    return <Square value={i} />;
   }
 
   render() {

861f539e57113e35b9260c2762cf88b4434bc81d
Zach Benson <zachzikool101@gmail.com>
2019-04-18 15:17:18 -0600

    edit

--- src/index.js
+++ src/index.js
@@ -23,22 +23,51 @@ class Board extends React.Component {
       turn : 'X'
     }
   }
+
+  get state() {
+    return {
+      squares: this.squares,
+      turn: this.turn
+    }
+  }
+
+  get square(i) {
+    return this.state.squares[i];
+  }
+
+  setSquare(i,value) {
+    let state = this.state;
+    state.squares[i]=value;
+    this.setState(state);
+  }
+
+  get squares() {
+    return this.state.squares.slice();
+  }
+  get turn() {
+    return this.state.turn;
+  }
+  set turn(value) {
+    let state = this.state
+    state.turn = value;
+    this.setState
+  }
   changeTurn(){
-    if(this.state.turn == 'X') {
-      this.state.turn = 'O';
+    if(this.turn == 'X') {
+      this.turn = 'O';
     } else {
-      this.state.turn = 'X';
+      this.turn = 'X';
     }
   }
   handelClick(i) {
-    if(this.state.squares[i] == null){
-      this.state.squares[i]=turn;
+    if(this.getSquare[i] == null){
+      this.setSquare(i)=this.turn;
       this.changeTurn();
   }
 }
   renderSquare(i) {
     return <Square value={this.state.squares[i]}
-                    onClick={()=>this.handleClick(i)} />;
+                    onClick={()=>this.handelClick(i)} />;
   }
 
   render() {
